3c23f3d7594250f8a541049eb794a18507a3f99d
fixfell7 <30707948+fixfell7@users.noreply.github.com>
2023-06-28 09:31:17 -0400

    Update HTML_PAGE_DATALOGGER.js

--- HTML_PAGE_DATALOGGER.js
+++ HTML_PAGE_DATALOGGER.js
@@ -4,36 +4,34 @@ let newWindow;
 
 function openDataWindow() {
   newWindow = window.open('', '_blank');
-  newWindow.document.write('<html><head><title>Data</title></head><body><table>');
-}
-
-function closeDataWindow() {
-  if (newWindow) {
-    newWindow.document.write('</table></body></html>');
-    newWindow.document.close();
-    newWindow = null;
-  }
+  newWindow.document.write('<html><head><title>Data Logger: ' + ELEMENT_ID + '</title></head></html><table>');
+  newWindow.document.write('<tr><td>Time</td><td>Temperature</td></tr>');
 }
 
 function appendDataToWindow(time, value) {
   if (newWindow) {
-    newWindow.document.write('<tr><td>' + time + ',</td><td>' + value + '</td></tr>');
+    newWindow.document.write('<tr><td>' + time + '</td><td>' + value + '</td></tr>');
   }
 }
-
 
 function monitorData() {
   const element = document.getElementById(ELEMENT_ID);
   if (element) {
-  
-	let date = new Date();
-	let hours = date.getHours();
-	let minutes = date.getMinutes();
-	let seconds = date.getSeconds();
-	const currentTime = hours + ':' + minutes + ':' + seconds
-	
-    const value = element.innerText;
+    
+    let date = new Date();
+    let hours = 	("0" + date.getHours()).slice (-2);
+    let minutes = 	("0" + date.getMinutes()).slice (-2);
+    let seconds = 	("0" + date.getSeconds()).slice (-2);
+    const currentTime = hours + ':' + minutes + ':' + seconds
+    //console.log("currentTime: " + currentTime)
+    
+    var value = element.innerText;
+    value = value.replace(/\./g, ',');
+    value = value.replace(/[^0-9,]/g, '');
     appendDataToWindow(currentTime, value);
+  }
+  else {
+    console.log("elemet ID " + ELEMENT_ID + " not found")
   }
 }
 
